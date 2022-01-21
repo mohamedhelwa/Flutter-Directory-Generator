5b716eca1974ac4a998ef893df4905af9092cf84
GitHub Action <action@github.com>
2022-01-21 10:02:50 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/mohamedhelwa/flutterdirectorygenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.mohamedhelwa.flutterdirectorygenerator.services
+
+import com.github.mohamedhelwa.flutterdirectorygenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
